4408d995027beb87651a68a41efcf1c17edd7674
Gajaveniravali <samravali22@gmail.com>
2026-06-19 12:32:36 +0530

    Add GitHub Actions workflow

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
